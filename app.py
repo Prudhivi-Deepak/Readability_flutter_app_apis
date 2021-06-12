77adf0de3c842e1330e40046c5bae9f011d45907
Prudhivi Deepak <81976931+Prudhivi-Deepak@users.noreply.github.com>
2021-06-12 17:49:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,5 +99,42 @@ def alldata():
 
     return jsonify({"output":dict1})
 
+@app.route('/user/<emailvalue>')
+def userdata(emailvalue):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM predict where email = %s", (str(emailvalue),))
+    response = cursor.fetchall()
+    dict1 = {}
+    headings=["automated_readability_index", "coleman_liau_index","crawford", 
+                        "dale_chall_readability_score", "difficult_words", "fernandez_huerta", 
+                        "flesch_kincaid_grade", "flesch_reading_ease", "gunning_fog", 
+                        "gutierrez_polini", "linsear_write_formula", "smog_index", 
+                        "szigriszt_pazos"]
+    for j,i in enumerate(response):
+        dict1[j]={
+                    "id":i[0],
+                    "time":i[1],
+                    "email":i[2],
+                    "text_input":i[3],
+                    "text_standard":i[4],
+                    "automated_readability_index":i[5],
+                    "coleman_liau_index":i[6],
+                    "crawford":i[7],
+                    "dale_chall_readability_score":i[8],
+                    "difficult_words":i[9],
+                    "fernandez_huerta":i[10],
+                    "flesch_kincaid_grade":i[11],
+                    "flesch_reading_ease":i[12],
+                    "gunning_fog":i[13],
+                    "gutierrez_polini":i[14],
+                    "linsear_write_formula":i[15],
+                    "smog_index":i[16],
+                    "szigriszt_pazos":i[17]
+                }        
+            
+    print(dict1)
+
+    return jsonify({"output":dict1})
+
 if __name__ == "__main__":
     app.run(debug=True)
