89056e846f8564658991c5f17e7d709ab0496d59
Prudhivi Deepak <81976931+Prudhivi-Deepak@users.noreply.github.com>
2021-06-12 15:22:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ import textstat
 import json
 from flask.json import jsonify
 from flask import Flask, redirect, url_for, session,render_template,request
+from flask_mysqldb import MySQL
+
+app.secret_key = os.urandom(24)
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+app.config['MYSQL_USER'] = 'iQnG3ACqz9'
+app.config['MYSQL_PASSWORD'] = '6reaeX1348'
+app.config['MYSQL_DB'] = 'iQnG3ACqz9'
+mysql = MySQL(app)
 
 app = Flask(__name__)
-@app.route('/<test_data>')
-def login(test_data):
-    if(len(test_data.split(" "))<50):
+@app.route('/<id>')
+def login(id):
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM upload WHERE id = %s", (str(id1),))
+    response = cursor.fetchall()
+    
+    if(len(response[-1][-1].split(" "))<50):
         return jsonify({"output":999}
+       
+    test_data = response[-1][-1]
                        
     print(textstat.flesch_reading_ease(test_data),"\n",
     textstat.smog_index(test_data),"\n",
@@ -26,20 +41,24 @@ def login(test_data):
 
     return jsonify({"output":
         {
-        "flesch_reading_ease":textstat.flesch_reading_ease(test_data),
-        "smog_index":textstat.smog_index(test_data),
-        "flesch_kincaid_grade":textstat.flesch_kincaid_grade(test_data),
-        "coleman_liau_index":textstat.coleman_liau_index(test_data),
-        "automated_readability_index":textstat.automated_readability_index(test_data),
-        "dale_chall_readability_score":textstat.dale_chall_readability_score(test_data),
-        "difficult_words":textstat.difficult_words(test_data),
-        "linsear_write_formula":textstat.linsear_write_formula(test_data),
-        "gunning_fog":textstat.gunning_fog(test_data),
-        "text_standard":textstat.text_standard(test_data),
-        "fernandez_huerta":textstat.fernandez_huerta(test_data),
-        "szigriszt_pazos":textstat.szigriszt_pazos(test_data),
-        "gutierrez_polini":textstat.gutierrez_polini(test_data),
-        "crawford":textstat.crawford(test_data)
+            "id":response[-1][0],
+            "email":response[-1][1],
+            "timne":response[-1][2],
+            "text_input":response[-1][3],
+            "flesch_reading_ease":textstat.flesch_reading_ease(test_data),
+            "smog_index":textstat.smog_index(test_data),
+            "flesch_kincaid_grade":textstat.flesch_kincaid_grade(test_data),
+            "coleman_liau_index":textstat.coleman_liau_index(test_data),
+            "automated_readability_index":textstat.automated_readability_index(test_data),
+            "dale_chall_readability_score":textstat.dale_chall_readability_score(test_data),
+            "difficult_words":textstat.difficult_words(test_data),
+            "linsear_write_formula":textstat.linsear_write_formula(test_data),
+            "gunning_fog":textstat.gunning_fog(test_data),
+            "text_standard":textstat.text_standard(test_data),
+            "fernandez_huerta":textstat.fernandez_huerta(test_data),
+            "szigriszt_pazos":textstat.szigriszt_pazos(test_data),
+            "gutierrez_polini":textstat.gutierrez_polini(test_data),
+            "crawford":textstat.crawford(test_data)
     }})
 
 if __name__ == "__main__":
